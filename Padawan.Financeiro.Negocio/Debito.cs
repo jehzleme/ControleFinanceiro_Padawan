c161c81774860269a8d33846fbb065980a988b85
Rômulo Barbosa Ribeiro <romulo.ribeiro@hbsis.com.br>
2020-09-01 15:20:46 -0300

    Criei debito

---
+++ Padawan.Financeiro.Negocio/Debito.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Padawan.Financeiro.Negocio
+{
+    class Debito
+    {
+        public double valor { get; set; }
+    }
+}
